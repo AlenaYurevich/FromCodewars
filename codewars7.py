a4829176e0ab9e8cde00e88dd38a49bc9d5b5d62
Alena Yurevich <yurevichei@mail.ru>
2023-07-05 20:20:55 +0300

    count and match

--- codewars7.py
+++ codewars7.py
@@ -18,11 +18,39 @@
 
 # Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings,
 # ordered from shortest to longest.
-def sort_by_length(arr):
-    arr.sort(key=len)
-    print(arr)
+# def sort_by_length(arr):
+#     arr.sort(key=len)
+#     print(arr)
+#
+#
+# sort_by_length(["iiiii", "to", "beg", "life"])
 
 
-sort_by_length(["iiiii", "to", "beg", "life"])
+# You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+# Complete the method which accepts such an array, and returns that single different number.
+# def stray(arr):
+#     for i in arr:
+#         if arr.count(i) == 1:
+#             print(i)
+#
+#
+# stray([17, 17, 3, 17, 17, 17, 17])
+# stray([1, 1, 2])
 
 
+# When provided with a number between 0-9, return it in words.
+def switch_it_up(number):
+    match number:
+        case 0: return "Zero"
+        case 1: return "One"
+        case 2: return "Two"
+        case 3: return "Three"
+        case 4: return "Four"
+        case 5: return "Five"
+        case 6: return "Six"
+        case 7: return "Seven"
+        case 8: return "Eight"
+        case 9: return "Nine"
+
+
+print(switch_it_up(8))
