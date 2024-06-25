d33e3c89d7d3cb87ceb08a1b9ea8aa85f96ee6e1
Alena <yurevichei@mail.ru>
2024-06-25 19:03:06 +0300

    match and case

--- codewars6.py
+++ codewars6.py
@@ -205,3 +205,25 @@ def warn_the_sheep(queue):
         return 'Pls go away and stop eating my sheep'
     else:
         return f"Oi! Sheep number {queue.index('wolf')}! You are about to be eaten by a wolf!"
+
+
+# You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other
+# items. We want to create the text that should be displayed next to such an item.
+#
+# Implement the function which takes an array containing the names of people that like an item. Itmustreturn the display
+# text as shown in the examples:
+#
+# []                                -->  "no one likes this"
+# ["Peter"]                         -->  "Peter likes this"
+# ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+# ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+# ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+
+def likes(names):
+    match names:
+        case []: return 'no one likes this'
+        case [a]: return f'{a} likes this'
+        case [a, b]: return f'{a} and {b} like this'
+        case [a, b, c]: return f'{a}, {b} and {c} like this'
+        case [a, b, *rest]: return f'{a}, {b} and {len(rest)} others like this'
