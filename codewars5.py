c823671c0d380224461186cdce970b1bddf95d40
Alena <yurevichei@mail.ru>
2024-08-13 19:23:28 +0300

    if else else else

--- codewars5.py
+++ codewars5.py
@@ -245,3 +245,21 @@ def how_much_i_love_you(nb_petals):
           5: "madly",
           0: "not at all"}
     return di[nb_petals % 6]
+
+
+"""
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month
+11 (November), is part of the fourth quarter.
+"""
+
+
+# def quarter_of(month):
+#     li = ["", "1 2 3", "4 5 6", "7 8 9", "10 11 12"]
+#     for mo in li:
+#         if str(month) in mo:
+#             return li.index(mo)
+
+
+def quarter_of(month):
+    return 1 if month <= 3 else 2 if month <= 6 else 3 if month <= 9 else 4
